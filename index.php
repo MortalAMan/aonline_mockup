d15282071e7cb90c37312cdd8a55a59f39e508ac
Addy <Ashepherdking@gmail.com>
2017-11-07 14:05:36 -0700

    head finished

--- index.php
+++ index.php
@@ -1,56 +1,64 @@
-<?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package AO_Base
- */
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-get_header(); ?>
+    <title>Blog Template for Bootstrap</title>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 
-		<?php
-		if ( have_posts() ) :
+  </head>
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+  <body>
 
-			<?php
-			endif;
+  <div class="row">
+    <div class="col"></div>
+    <div class="col"></div>
+    <div class="col"></div>
+    <div class="col"><img src="<?php echo get_template_directory_uri() . '/images/logo.jpg'; ?>" class="col" /></div>
+  </div>
+  
+  <div class="container">
+		<div class="row no-gutters">
+        <img src="<?php echo get_template_directory_uri() . '/images/constructionworkers.jpg'; ?>" class="col-6 col-md-4" />
+        <img src="<?php echo get_template_directory_uri() . '/images/pinklady.jpg'; ?>" class="col-6 col-md-4" />
+        <img src="<?php echo get_template_directory_uri() . '/images/ladyveg.jpg'; ?>" class="col-6 col-md-4" />
+    </div>
+    <div class="row no-gutters">
+        <img src="<?php echo get_template_directory_uri() . '/images/apronman.jpg'; ?>" class="col-6 col-md-4" />
+        <img src="<?php echo get_template_directory_uri() . '/images/sawlady.jpg'; ?>" class="col-6 col-md-4" />
+        <img src="<?php echo get_template_directory_uri() . '/images/curlydark.jpg'; ?>" class="col-6 col-md-4" />
+    </div>
+  </div>
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+  <blockquote class="blockquote text-center">
+    <h1 class="display-4">Keep The Michigan First Energy Law</h1>
+  </blockquote>
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
 
-			endwhile;
+  
 
-			the_posts_navigation();
+	
+    
+    
+  
+      
+  
 
-		else :
+    <footer class="blog-footer">
+    </footer>
 
-			get_template_part( 'template-parts/content', 'none' );
 
-		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
