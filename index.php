5f913501ee94edbb6c6b7596213dc910a7ff7c95
Addy <Ashepherdking@gmail.com>
2017-11-08 20:28:59 -0700

    finished test

--- index.php
+++ index.php
@@ -84,15 +84,15 @@
     </blockquote>
   </div>
 
-<div class="col-8 mx-auto">
-  <div class="card mt-5 mb-5">
-    <div class="card-body bg-warning">
-      <div class="text-center text-primary">
-        <h3>The New Michigan First Energy law ensures Michigan's energy is:</h3>
+  <div class="col-8 mx-auto">
+    <div class="card mt-5 mb-5">
+      <div class="card-body bg-warning">
+        <div class="text-center text-primary">
+          <h3>The New Michigan First Energy law ensures Michigan's energy is:</h3>
+        </div>
       </div>
     </div>
   </div>
-</div>
   
   <div class="container">
     <div class="row blockquote text-center">
@@ -105,13 +105,22 @@
     </div>
   </div>
 
-  <div class="card mx-auto mt-5 mb-5 w-75">
-    <div class="card-body bg-primary">
-      <div class="text-center text-white">
-        <h2></h2>
-      </div>
+  <div class="container">
+    <div class="row blockquote text-center">
+      <div class="col"><img src="<?php echo get_template_directory_uri() . '/images/footer.jpg'; ?>" class="col-lg-12 mb-5 mt-4" /></div>
     </div>
   </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+  
 
 
 
@@ -142,15 +151,3 @@
       
   
 
-    <footer class="blog-footer">
-    </footer>
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-  </body>
-</html>
